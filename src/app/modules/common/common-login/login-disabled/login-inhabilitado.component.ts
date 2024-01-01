816fc49c1de403bbd18b34da31fd0b7e65e07ec8
franncoherrera <franncoherrera2011@gmail.com>
2024-01-01 19:18:11 -0300

    feat: login disabled finished

--- src/app/modules/common/common-login/login-disabled/login-inhabilitado.component.ts
+++ src/app/modules/common/common-login/login-disabled/login-inhabilitado.component.ts
@@ -3,8 +3,12 @@ import { Router } from '@angular/router';
 import { LoginService } from '../login-services/login.service';
 import { Observable, catchError, map, of } from 'rxjs';
 import { AlertsService } from 'src/app/helpers/alerts.service';
-import { common_error, common_login } from 'src/app/transalation/es/COMMON_MESSAGES_es';
+import {
+  common_error,
+  common_login,
+} from 'src/app/transalation/es/COMMON_MESSAGES_es';
 import { general_path } from 'src/app/constants/ROUTES';
+import { icon_class } from 'src/assets/icons_class/icon_class';
 
 @Component({
   selector: 'app-login-disabled',
@@ -13,7 +17,8 @@ import { general_path } from 'src/app/constants/ROUTES';
 })
 export class LoginDisabledComponent implements OnInit {
   reasonReports$: Observable<string[]>;
-  common_login = common_login
+  common_login = common_login;
+  icon_class = icon_class;
   constructor(
     private router: Router,
     private loginService: LoginService,
--- src/assets/icons_class/icon_class.ts
+++ src/assets/icons_class/icon_class.ts
@@ -1,4 +1,5 @@
 export const icon_class = {
     eye_open: 'fas fa-eye',
-    eye_close: 'fas fa-eye-slash'
+    eye_close: 'fas fa-eye-slash',
+    exclamation_signal: 'fa-solid fa-circle-exclamation'
 }
